dfe65dbc5852887dbf171f584e38879d459d7359
Khaled Ahmed <kmhsn217@gmail.com>
2024-05-24 19:04:41 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Khaled Mohsen on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
